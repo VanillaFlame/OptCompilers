b68a4694b5b0f79be110c6adc762e4286ce86809
Alexandr Pogorelov <Deadgirlneversayno@gmail.com>
2020-04-28 13:16:46 +0300

    Revert "living and dead variables"

--- OptCompilers-master/TACode/TacNodes/TacIfGotoNode.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleLang.TACode.TacNodes
-{
-    public class TacIfGotoNode : TacGotoNode
-    {
-        public string Condition { get; set; }
-
-        public override string ToString() => (Label != null ? Label + ": " : "") + $"if {Condition} goto {TargetLabel}";
-    }
-}
