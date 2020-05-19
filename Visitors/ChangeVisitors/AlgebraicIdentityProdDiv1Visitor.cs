8bf031df75a81dba5c35850db8d83a754f957113
manger32 <manger32@gmail.com>
2020-05-19 15:07:05 +0300

    Add files via upload
    Оптимизации по синт. дереву - 1 * ex, ex * 1, ex /1; ex + 0

---
+++ Visitors/ChangeVisitors/AlgebraicIdentityProdDiv1Visitor.cs
@@ -0,0 +1,41 @@
+﻿using ProgramTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang.Visitors
+{
+    class AlgebraicIdentityProdDiv1Visitor : ChangeVisitor
+    {
+        public override void Visit(BinExprNode BinOp)
+        {
+            switch (BinOp.OpType)
+            {
+                case BinOpType.Prod:
+                    if (BinOp.Left is IntNumNode && (BinOp.Left as IntNumNode).Num == 1)
+                    {
+                        BinOp.Right.Visit(this);
+                        ReplaceExpr(BinOp, BinOp.Right);
+                    }
+                    else if (BinOp.Right is IntNumNode && (BinOp.Right as IntNumNode).Num == 1)
+                    {
+                        BinOp.Left.Visit(this);
+                        ReplaceExpr(BinOp, BinOp.Left);
+                    }
+                    else base.Visit(BinOp);
+                    break;
+                case BinOpType.Div:
+                    if (BinOp.Right is IntNumNode && (BinOp.Right as IntNumNode).Num == 1)
+                    {
+                        BinOp.Left.Visit(this);
+                        ReplaceExpr(BinOp, BinOp.Left);
+                    }
+                    break;
+                default:
+                    base.Visit(BinOp);
+                    break;
+            }
+        }
+    }
+}
---
+++ Visitors/ChangeVisitors/AlgebraicIdentitySum0Visitor.cs
@@ -0,0 +1,26 @@
+﻿using ProgramTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang.Visitors.ChangeVisitors
+{
+    class AlgebraicIdentitySum0Visitor : ChangeVisitor
+    {
+        public override void Visit(BinExprNode BinOp)
+        {
+            if (BinOp.Left is IntNumNode && (BinOp.Left as IntNumNode).Num == 0)
+            {
+                BinOp.Right.Visit(this);
+                ReplaceExpr(BinOp, BinOp.Right);
+            }
+            else if (BinOp.Right is IntNumNode && (BinOp.Right as IntNumNode).Num == 0)
+            {
+                BinOp.Left.Visit(this);
+                ReplaceExpr(BinOp, BinOp.Left);
+            }
+            else base.Visit(BinOp);
+        }
+    }
+}
