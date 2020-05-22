2952ab1c31d53d8affc1edf718fd6ab982cb7d1e
1xVinniThePuh <1xVinniThePuh@users.noreply.github.com>
2020-05-22 15:10:52 +0300

    опт. по дереву

---
+++ Visitors/ChangeVisitors/AlwaysIfOrElseVisitor.cs
@@ -0,0 +1,29 @@
+﻿using ProgramTree;
+
+namespace SimpleLang.Visitors.ChangeVisitors
+{
+    class AlwaysIfOrElseVisitor : ChangeVisitor
+    {
+        public override void Visit(IfNode ifnode)
+        {
+            if (ifnode.Condition is BoolValNode expr)
+            {
+                if (expr.Val == true)
+                {
+                    ReplaceStatement(ifnode, ifnode.Stat);
+                    base.Visit(ifnode.Stat);
+                }
+                else
+                {
+                    ReplaceStatement(ifnode, ifnode.ElseStat);
+                    base.Visit(ifnode.ElseStat);
+                }
+            }     
+            else
+            {
+                base.Visit(ifnode);
+            }
+        }
+
+    }
+}
---
+++ Visitors/NumberEqualityVisitor.cs
@@ -0,0 +1,19 @@
+﻿using ProgramTree;
+
+namespace SimpleLang.Visitors.ChangeVisitors
+{
+    class NumberEqualityVisitor : ChangeVisitor
+    {
+        public override void Visit(BinExprNode binExpr)
+        {
+            base.Visit(binExpr);
+            if (binExpr.Left is IntNumNode left && binExpr.Right is IntNumNode right)
+            {
+                if (binExpr.OpType == BinOpType.Equal)
+                    ReplaceExpr(binExpr, new BoolValNode(left.Num == right.Num));
+                if (binExpr.OpType == BinOpType.NotEqual)
+                    ReplaceExpr(binExpr, new BoolValNode(left.Num != right.Num));
+            } 
+        }
+    }
+}
