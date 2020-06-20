13b0704f40d0efa66ac8ebff2bc5ad4177f6a145
Александр Погорелов <wowitsmylife@rambler.ru>
2020-06-20 11:28:08 +0300

    Natural loops

---
+++ TAC/NaturalLoop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLang.TAC
+{
+    public class NaturalLoop
+    {
+        public BasicBlock Start { get; set; }
+
+        public BasicBlock End { get; set; }
+
+        public List<BasicBlock> Blocks { get; set; }
+
+        public NaturalLoop(BasicBlock start, BasicBlock end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public void AddBlock(BasicBlock block)
+        {
+            Blocks.Add(block);
+        }
+    }
+}
