8ebfa13254507f6ff28ed4e9c5259c3e31814ded
Домбровская Анна <anna.dombrowskaya@gmail.com>
2020-06-22 10:39:57 +0300

    Тесты DefUseOptimizer

---
+++ TestSuite/TAC/DefUseOptimizerTests.cs
@@ -0,0 +1,107 @@
+﻿using SimpleLang.TACOptimizers;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestSuite.TAC
+{
+    [TestFixture]
+    class DefUseOptimizerTests : TACTestsBase
+    {
+        [Test]
+        public void SimpleExample()
+        {
+            var TAC = GenerateTAC(
+@"
+{
+  a = b;
+  a = c;
+}
+");
+            var optimizer = new DefUseOptimizer(TAC);
+            optimizer.Run();
+            var expected = new List<string>()
+            {
+                "",
+               "a = c"
+            };
+            var actual = optimizer.TAC.Instructions.Select(instruction => instruction.ToString().Trim());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void SimpleExample2()
+        {
+            var TAC = GenerateTAC(
+@"
+{
+  b = d;
+  a = b;
+  a = c;
+}
+");
+            var optimizer = new DefUseOptimizer(TAC);
+            optimizer.Run();
+            var expected = new List<string>()
+            {
+               "b = d",
+                "",
+               "a = c"
+            };
+            var actual = optimizer.TAC.Instructions.Select(instruction => instruction.ToString().Trim());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void NothingToDelete()
+        {
+            var TAC = GenerateTAC(
+@"
+{
+  a = b;
+  b = c;
+  c = x + y;
+}
+");
+            var optimizer = new DefUseOptimizer(TAC);
+            optimizer.Run();
+            var expected = new List<string>()
+            {
+               "a = b",
+               "b = c",
+               "#t0 = x + y",
+               "c = #t0"
+            };
+            var actual = optimizer.TAC.Instructions.Select(instruction => instruction.ToString().Trim());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void DeleteTempVariable()
+        {
+            var TAC = GenerateTAC(
+@"
+{
+  a = b + d;
+  b = c;
+  a = b + d;
+}
+");
+            var optimizer = new DefUseOptimizer(TAC);
+            optimizer.Run();
+            var expected = new List<string>()
+            {
+               "",
+               "",
+               "b = c",
+               "#t1 = b + d",
+               "a = #t1"
+            };
+            var actual = optimizer.TAC.Instructions.Select(instruction => instruction.ToString().Trim());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
