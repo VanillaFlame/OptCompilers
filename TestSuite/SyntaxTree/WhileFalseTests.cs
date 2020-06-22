fa7db2b2e8d0161d855b6d5870b537ff50b6ce29
Гуртовой Александр Игоревич <sanyarnd313@gmail.com>
2020-06-22 10:44:12 +0300

    Tests for WhileFalseVisitor

---
+++ TestSuite/SyntaxTree/WhileFalseTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SimpleLang;
+using SimpleLang.TAC;
+using SimpleLang.Visitors;
+using SimpleParser;
+using SimpleScanner;
+using SimpleLang.Visitors.ChangeVisitors;
+using TestSuite.TAC;
+using ProgramTree;
+
+
+namespace TestSuite.SyntaxTree
+{
+    [TestFixture]
+    class WhileFalseTests : TACTestsBase
+    {
+        [Test]
+        public void Test1()
+        {
+            var Text =
+@"
+{
+  a = 3;
+  while (false)
+  {
+    b = a;
+  }
+}
+";
+            Scanner scanner = new Scanner();
+            scanner.SetSource(Text, 0);
+            Parser parser = new Parser(scanner);
+            parser.Parse();
+            var parentFiller = new FillParentsVisitor();
+            parser.root.Visit(parentFiller);
+
+            var trueIfOpt = new WhileFalseVisitor();
+            parser.root.Visit(trueIfOpt);
+
+            var prettyPrinter = new PrettyPrinterVisitor();
+            parser.root.Visit(prettyPrinter);
+
+            var TACGenerator = new TACGenerationVisitor();
+            parser.root.Visit(TACGenerator);
+            var TAC = TACGenerator.TAC;
+
+            var expected = new List<string>()
+            {
+                "a = 3"
+            };
+            var actual = TAC.Instructions.Select(instruction => instruction.ToString().Trim());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var Text =
+@"
+{
+  b = 4;
+  while (false)
+  {
+    b = b;
+  }
+  a = 33;
+}
+";
+            Scanner scanner = new Scanner();
+            scanner.SetSource(Text, 0);
+            Parser parser = new Parser(scanner);
+            parser.Parse();
+            var parentFiller = new FillParentsVisitor();
+            parser.root.Visit(parentFiller);
+
+            var whileFalse = new WhileFalseVisitor();
+            parser.root.Visit(whileFalse);
+
+            var prettyPrinter = new PrettyPrinterVisitor();
+            parser.root.Visit(prettyPrinter);
+
+            var TACGenerator = new TACGenerationVisitor();
+            parser.root.Visit(TACGenerator);
+            var TAC = TACGenerator.TAC;
+
+            var expected = new List<string>()
+            {
+               	"b = 4",
+                "a = 33"
+            };
+            var actual = TAC.Instructions.Select(instruction => instruction.ToString().Trim());
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+} 
